43572e94f6cf7995bc783b122472befe9a07d69c
Alexey <o.pidckuycko2011@gmail.com>
2022-08-17 00:43:26 +0300

    auth

--- src/features/auth/components/form-sign-in.tsx
+++ src/features/auth/components/form-sign-in.tsx
@@ -1,23 +1,55 @@
 import { Helmet } from "react-helmet";
+import { useForm } from "react-hook-form";
+import { Form, FormControl, FormLabel, FormGroup, Modal, ModalBody, ModalHeader, ModalTitle, Container, Button, FormText } from "react-bootstrap";
 
 function FormSingIn(){
-    
+    const {
+         register, 
+         handleSubmit, 
+         formState: { errors } 
+        } = useForm();
+        
+    const onSubmit = () =>{
+        console.log("to phone book ....")
+    }
     return(
         <>
             <Helmet>
                 <title>Log In</title>
             </Helmet>
-            <form>
-                <label htmlFor = "email"> Email:</label>
-                <input name = "email"/>
-                <br/>
-                <label htmlFor = "password">Password:</label>
-                <input name = "password"/>
-                <br/>
-                <button type = "submit">Sing in</button>
-            </form>
-    </>     
-            
+            <Container style = {{ width:"18rem"}}>
+                <Form onSubmit = {handleSubmit(onSubmit)}>
+                    <FormGroup>
+                        <FormLabel>Email adress:</FormLabel>
+                        <FormControl
+                            {...register('emailAdress',{
+                                required: "Must be filled",
+                            })}
+                        ></FormControl> 
+                        <FormText style = {{color:"red"}}>
+                            {errors?.emailAdress && <p>{errors?.emailAdress?.message || "Error!"}</p>}
+                        </FormText>
+                    </FormGroup>
+                    <FormGroup>
+                        <FormLabel>Password:</FormLabel>
+                        <FormControl
+                             {...register('password',{
+                                required: "Must be filled",
+                                minLength: {
+                                    value:8,
+                                    message:"Minimum 8 symbols"
+                                }
+                            })}
+                        ></FormControl>
+                        <FormText style = {{color:"red"}}>
+                            {errors?.password && <p>{errors?.password?.message || "Error!"}</p>}
+                        </FormText>
+                    </FormGroup>
+                    <Button type = "submit" variant="outline-info" style = {{ marginTop:"5px"}}> Log In</Button>
+                </Form>
+            </Container>
+                
+        </>   
     )
 }
 export default FormSingIn
--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router-dom';
-import  { Navbar, Nav, Button } from 'react-bootstrap';
+import  { Navbar, Nav, Button, NavbarBrand } from 'react-bootstrap';
 
 export default function Layout(){
     return (
         <>  
-            <Navbar  bg="primary">
-                <h1>Phone books</h1>
-                <Nav >
-                    <Button variant="primery" className = "mr-2">Log in</Button>
+            <Navbar  bg="primary" style = {{ paddingLeft:"1rem",paddingRight:"1rem", marginBottom:"10px", display:"flex", justifyContent: "space-between"}}>
+                <NavbarBrand >
+                    Phone books
+                </NavbarBrand>
+                <Nav>
+                    <Button variant="primery">Log in</Button>
                     <Button variant="primery">Sing out</Button>
                 </Nav>
             </Navbar>
