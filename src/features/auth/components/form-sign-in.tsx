c9b7058752d06d6e2a337d4f0b34aeb42b8ebab7
Alexey <o.pidckuycko2011@gmail.com>
2022-08-19 12:02:58 +0300

    Auth

--- src/features/auth/components/form-sign-in.tsx
+++ src/features/auth/components/form-sign-in.tsx
@@ -1,70 +1,54 @@
-import { useForm } from "react-hook-form";
-import {
-  Form,
-  FormControl,
-  FormLabel,
-  FormGroup,
-  Container,
-  Button,
-  FormText,
-} from "react-bootstrap";
-import useAuth from "../hooks/use-auth";
+import { useForm } from 'react-hook-form';
+import { Form, FormControl, FormLabel, FormGroup, Container, Button, FormText } from 'react-bootstrap';
+import useAuth from '../hooks/use-auth';
 
 type FormValues = {
   password: string;
   emailAddress: string;
 };
+const a = 5;
 function FormSingIn() {
-  const { onLogin, isAuth } = useAuth();
+  const { onLogin } = useAuth();
   const {
     register,
     handleSubmit,
     formState: { errors, isValid },
   } = useForm<FormValues>({
-    mode: "onChange",
+    mode: 'onChange',
   });
 
   const onSubmit = () => onLogin();
   return (
     <>
-      <Container style={{ width: "18rem" }}>
+      <Container style={{ width: '18rem' }}>
         <Form onSubmit={handleSubmit(onSubmit)}>
           <FormGroup>
             <FormLabel>Email address:</FormLabel>
             <FormControl
-              {...register("emailAddress", {
+              {...register('emailAddress', {
                 required: true,
                 pattern: /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/,
               })}
             ></FormControl>
-            <FormText style={{ color: "red" }}>
-              {errors?.emailAddress && <p>Must be filled.Invalid email.</p>}
-            </FormText>
+            <FormText style={{ color: 'red' }}>{errors?.emailAddress && <p>Must be filled.Invalid email.</p>}</FormText>
           </FormGroup>
           <FormGroup>
             <FormLabel>Password:</FormLabel>
             <FormControl
-              {...register("password", {
+              {...register('password', {
                 required: true,
-                pattern:
-                  /^(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+                pattern: /^(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
               })}
             ></FormControl>
-            <FormText style={{ color: "red" }}>
+            <FormText style={{ color: 'red' }}>
               {errors?.password && (
                 <p>
-                  Must be filled.Minimum 8 symbols. Necessarily one number, one
-                  uppercase letter, one lowercase letter.
+                  Must be filled.Minimum 8 symbols. Necessarily one number, one uppercase letter, one lowercase letter.
                 </p>
               )}
             </FormText>
           </FormGroup>
-          <Button
-            type="submit"
-            disabled={!isValid}
-            variant="primary"
-            style={{ marginTop: "5px" }}
-          >
+          <Button type="submit" disabled={!isValid} variant="primary" style={{ marginTop: '5px' }}>
             Log In
           </Button>
         </Form>
