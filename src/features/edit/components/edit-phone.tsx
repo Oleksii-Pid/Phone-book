a709434c545afc6ae3f9fb660beddb14e5161ce1
Alexey <o.pidckuycko2011@gmail.com>
2022-08-19 15:28:29 +0300

    A

--- src/features/edit/components/edit-phone.tsx
+++ src/features/edit/components/edit-phone.tsx
@@ -1,13 +1,10 @@
-import { useParams } from "react-router-dom";
-import { Helmet } from "react-helmet";
+import { useParams } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 function EditPhone() {
   const { id } = useParams();
   return (
     <>
-      <Helmet>
-        <title>Edit</title>
-      </Helmet>
       <p>Edit number {id}</p>
     </>
   );
